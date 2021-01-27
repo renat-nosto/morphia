fbe924beb6dde6975b825af11445e46e49eb6b2e
evanchooly <jlee@antwerkz.com>
2021-01-26 22:34:12 -0500

    type is never null

--- core/src/main/java/dev/morphia/mapping/validation/fieldrules/ReferenceToUnidentifiable.java
+++ core/src/main/java/dev/morphia/mapping/validation/fieldrules/ReferenceToUnidentifiable.java
@@ -3,7 +3,6 @@ package dev.morphia.mapping.validation.fieldrules;
 import dev.morphia.Key;
 import dev.morphia.annotations.Reference;
 import dev.morphia.mapping.Mapper;
-import dev.morphia.mapping.MappingException;
 import dev.morphia.mapping.codec.pojo.EntityModel;
 import dev.morphia.mapping.codec.pojo.PropertyModel;
 import dev.morphia.mapping.validation.ConstraintViolation;
@@ -20,11 +19,7 @@ public class ReferenceToUnidentifiable extends PropertyConstraint {
     @Override
     protected void check(Mapper mapper, EntityModel entityModel, PropertyModel propertyModel, Set<ConstraintViolation> ve) {
         if (propertyModel.hasAnnotation(Reference.class)) {
-            final Class realType = /*(mf.isScalarValue()) ? mf.getType() : */propertyModel.getNormalizedType();
-
-            if (realType == null) {
-                throw new MappingException(Sofia.nullPropertyType(propertyModel));
-            }
+            final Class realType = propertyModel.getNormalizedType();
 
             if (realType.equals(Key.class)) {
                 ve.add(new ConstraintViolation(Level.FATAL, entityModel, propertyModel, getClass(), Sofia.keyNotAllowedAsProperty()));
